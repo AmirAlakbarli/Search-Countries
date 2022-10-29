69bf87ba483435cea7999e24b46e9a2f652eb581
AmirAlakbarli <emirelekberli@gmail.com>
2022-10-29 23:19:17 +0400

    focus problem with search input has been solved

--- my-app/src/components/Form/Form.js
+++ my-app/src/components/Form/Form.js
@@ -1,17 +1,20 @@
-import React from "react";
+import { React, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./form.scss";
 
 const Form = ({ inputValue, setInputValue }) => {
+  const inputRef = useRef();
+  const handleFocus = () => inputRef.current.focus()
   const handleSubmit = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} onClick={handleFocus}>
       <label htmlFor="search">
         <FaSearch />
       </label>
       <input
+        ref={inputRef}
         id="search"
         type="text"
         value={inputValue}
